7cab4a7a4575aecff106da191dc0a169e994492f
Max Fresonke <max@fresonke.com>
2016-12-02 18:08:03 -0500

    update

--- GUIComponents.java
+++ GUIComponents.java
@@ -1,11 +1,15 @@
 import javax.swing.*;
-public class GUIComponents {
+import java.awt.*;
+import java.awt.event.*; 
+public class GUIComponents 
+{
 
-	public static void main(String[] args) {
+	public static void main(String[] args) 
+	{
 		// JButton jbtOK = new JButton("OK");
 		// JButton jbtCancel = new JButton("Cancel");
 		
-		JLabel labelAdj = new JLabel("ADJECTIVE")
+		JLabel labelAdj = new JLabel("ADJECTIVE");
 		JTextField textAdj = new JTextField("smelly");
 
 		JLabel labelNoun = new JLabel("NOUN");
@@ -18,45 +22,16 @@ public class GUIComponents {
 		JTextField textNoise = new JTextField("screeeeech");
 
 
-		JButton btn = new JButton("HERE WE GO!!!")
-		JTextField result = new JTextField("Wait for it...");
+		JButton btn = new JButton("HERE WE GO!!!");
+		JTextArea result = new JTextArea("Wait for it...");
 
 		// Create Panel
 
-		// Set that magical thing to make it all one line
-
-		// Add items to panel
-
-		// Add action listener
-
-		btn.addActionListener(new ActionListener() {
-			String adj = textAdj.getText();
-			String nou = textNoun.getText();
-			String ani = textAnimal.getText();
-			String noi = textNoise.getText();
-
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                 result.setText(
-					"AJDC Macdonald had a " + adj + ", E-I-E-I-O\n" + 
-					"and on that " + nou + " he had a " + ani + ", E-I-E-I-O\n" +
-                );
-				// with a NOUN NOUN here
-				// and a NOUN NOUN there,
-				// here a NOUN, there a NOUN,
-				// everywhere a NOUN NOUN,
-				// AJDC Macdonald had a NOUN, E-I-E-I-O.	
-            }
-        });
-
-		
-
-
-		
-
-			)
-		
 		JPanel panel = new JPanel();
+		// Set that magical thing to make it all one line
+		panel.setLayout(new BoxLayout(panel, BoxLayout.PAGE_AXIS));
+		
+		// Add items to panel
 		panel.add(labelAdj);
 		panel.add(textAdj);
 		panel.add(labelNoun);
@@ -65,14 +40,41 @@ public class GUIComponents {
 		panel.add(textAnimal);
 		panel.add(labelNoise);
 		panel.add(textNoise);
-		
+		panel.add(btn);
+		panel.add(result);
+
+		//Add JFrame
 		JFrame frame = new JFrame();
 		frame.add(panel);
 		frame.setTitle("Show GUI components");
-		frame.setSize(450, 100);
+		frame.setSize(450, 1000);
 		frame.setLocation(200, 100);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.setVisible(true);
+
+				// Add action listener
+				btn.addActionListener(new ActionListener()
+		{
+
+            @Override
+            public void actionPerformed(ActionEvent e) 
+            {
+            	String adj = textAdj.getText();
+				String nou = textNoun.getText();
+				String ani = textAnimal.getText();
+				String noi = textNoise.getText();
+                 result.setText(
+					adj + " Macdonald had a " + nou + ", E-I-E-I-O\n" + 
+					"and in that " + nou + " he had a " + ani + ", E-I-E-I-O\n" +
+	               	"with a "+ noi + " " + noi +" here\n" +
+					"and a " + noi + " " + noi + " there,\n" +
+					"here a " + noi + ", there a " +  noi + ",\n" +
+					"everywhere a " + noi + " " +  noi + ",\n" +
+					adj + "Macdonald had a " + nou + ", E-I-E-I-O.");
+				
+        	}
+		});
+
 		
 	}
 
